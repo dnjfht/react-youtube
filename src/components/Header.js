0871165a23a11ab8af79f448a00253cf3fc7b730
Yoo Seungmin <dnjfht@gmail.com>
2023-05-25 17:44:24 +0900

    Tailwind 스타일링 - 삼항연산자

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillYoutube } from "react-icons/ai";
 import { ImYoutube2 } from "react-icons/im";
 import { IoIosSearch } from "react-icons/io";
 import { BsFillKeyboardFill } from "react-icons/bs";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Header() {
   const [appear, setAppear] = useState(false);
+  const [title, setTitle] = useState("");
 
   const navigate = useNavigate();
+  const { keyword } = useParams();
+
+  const handleChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleClickSearchBtn = (e) => {
+    e.preventDefault();
+
+    navigate(`/videos/${title}`);
+  };
+
+  useEffect(() => {
+    setTitle(keyword || "");
+  }, [keyword]);
 
   return (
     <div className=" w-100% h-[3.75rem] bg-[#0f0f0f] mx-auto">
@@ -23,44 +39,36 @@ export default function Header() {
           <ImYoutube2 className="text-6xl text-white" />
         </div>
         <div className="flex flex-row items-center mx-auto">
-          {appear ? (
-            <div className="relative">
-              <IoIosSearch className="absolute top-[50%] my-[-10px] mx-[16px] text-[20px] text-slate-300" />
-              <input
-                type="text"
-                placeholder="검색"
-                className="w-[36rem] h-10 bg-[#121212] rounded-l-full pr-1 pl-12 box-border border-solid border-[1px] border-[#4a7dff] outline-none placeholder:text-[#888888] text-white"
-                onFocus={() => {
-                  setAppear(true);
-                }}
-                onBlur={() => {
-                  setAppear(false);
-                }}
-              />
-
-              <BsFillKeyboardFill className="absolute top-[50%] mt-[-8px] right-2.5 text-[#888888]" />
-            </div>
-          ) : (
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="검색"
-                className="w-[33.75rem] h-10 bg-[#121212] rounded-l-full pr-1 pl-4 box-border border-solid border-[1px] border-[#ffffff36] outline-none placeholder:text-[#888888] text-white"
-                onFocus={() => {
-                  setAppear(true);
-                }}
-                onBlur={() => {
-                  setAppear(false);
-                }}
-              />
-
-              <BsFillKeyboardFill className="absolute top-[50%] mt-[-8px] right-2.5 text-[#888888]" />
-            </div>
-          )}
+          <div className="relative">
+            <IoIosSearch
+              className={`${
+                appear ? "block" : "hidden "
+              } absolute top-[50%] mt-[-10px] left-[15px] text-[20px] text-[#bebebe]`}
+            />
+            <input
+              type="text"
+              placeholder="검색"
+              className={`${
+                appear
+                  ? "w-[576px] pl-[48px] border-[#4a7dff]"
+                  : "w-[540px] pl-[16px] border-[rgb(203 213 225)]"
+              } h-10 bg-[#121212] rounded-l-full pr-1 box-border border-solid border-[1px] outline-none placeholder:text-[#888888] text-white`}
+              onFocus={() => {
+                setAppear(true);
+              }}
+              onBlur={() => {
+                setAppear(false);
+              }}
+              onChange={handleChangeTitle}
+              value={title}
+            />
+            <BsFillKeyboardFill className="absolute top-[50%] mt-[-8px] right-2.5 text-[#888888]" />
+          </div>
 
           <button
             type="button"
             className="w-[4rem] h-[2.5rem] bg-[#ffffff14] px-[1px] py-[6px] rounded-r-full box-border flex flex-row justify-center items-center border-solid border-[1px] border-[#ffffff36]"
+            onClick={handleClickSearchBtn}
           >
             <IoIosSearch className="text-[1.5rem] text-white" />
           </button>
--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>Not Found Page!</h1>
-      <p>707 ERROR</p>
+    <div className="w-[100%] h-[calc(100vh-60px)] bg-[#0f0f0f] overflow-y-scroll text-white">
+      <div className="w-[81.25rem] mx-auto p-10">
+        <h1>Not Found Page!</h1>
+        <p>707 ERROR</p>
+      </div>
     </div>
   );
 }
