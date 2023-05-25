9a05d9e96e93e7e518e9fec85b6f265711fc6d37
Yoo Seungmin <dnjfht@gmail.com>
2023-05-25 11:13:48 +0900

    Router 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "../../learn-router/src/pages/Home";
+import NotFoundPage from "../../learn-router/src/pages/NotFoundPage";
+import Detail from "./pages/Detail";
+
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    { path: "/", element: <Home />, errorElement: <NotFoundPage /> },
+    { path: "/:id", element: <Detail />, errorElement: <NotFoundPage /> },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1>Not Found Page!</h1>
+      <p>707 ERROR</p>
+    </div>
+  );
+}
